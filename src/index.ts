6d14c9e1d6b556258ff8776542897bbd0e37c862
Grazi <graziribeiro95@gmail.com>
2024-12-14 19:52:26 +0100

    add a session  and hashed example

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,41 @@ dotenv.config();
 import express from 'express';
 import helmet from 'helmet';
 import { urlRoute } from './url/url.route';
-
+import session from 'express-session';
+import { pool } from './db';
+import bcrypt from 'bcrypt';
 
 const app = express();
-const PORT = `${process.env.PORT}`;
+const PORT = process.env.PORT;
 
 app.use(helmet());
 app.use(express.json());
+
+if(!process.env.COOKIE_SESSION_SECRET) {
+    throw Error('Cookie session not found.')
+}
+
+app.use(session({
+    store: new (require('connect-pg-simple')(session))({
+        pool: pool,
+        tableName: "users_sessions",
+        createTableIfMissing: true
+    }),
+    secret: process.env.COOKIE_SESSION_SECRET,
+    resave: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+}));
+
+app.get('/login',(req, res) => {
+    (req.session as any).username = 'Grazi';
+    res.send();
+})
+
 app.use(urlRoute);
 
 app.listen(PORT, () => {
     console.log('Server Ready');
 })
+
+const hashedString = bcrypt.hashSync('eat', 10);
+console.log(bcrypt.compareSync('eat', hashedString));
