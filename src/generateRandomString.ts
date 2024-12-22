a5bec41bc082c4f8242256c1e54c09bc339ee361
Grazi <graziribeiro95@gmail.com>
2024-12-22 12:39:50 +0100

    replace generateRandom to use nanoId library

--- src/generateRandomString.ts
+++
@@ -1,9 +0,0 @@
-export default function generateRandomString(length: number) {
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-      result += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-    }
-    return result;
-}
-  
